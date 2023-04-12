3742f41900b57e07f96728215c8d00b83ecff4c5
Hanieh Khalesi <khalesihanieh@gmail.com>
2023-04-12 21:39:21 +0330

    export data to csv file

--- database_connect.py
+++ database_connect.py
@@ -1,5 +1,5 @@
 # make database sqlite3 and connect python to sqlite3 database using the ‘db-sqlite3‘ module.
-
+import csv
 import sqlite3
 
 
@@ -19,7 +19,23 @@ def select_all_records(cursor):
     for row in cursor.execute("SELECT * FROM feature_home"):
         print(row)
         
-        
+
+def export_database_to_csv_file(file_name, data):       
+    try:
+        with open(file_name, 'w', newline='')as f:
+            writer = csv.writer(f)
+            writer.writerow([
+                'Number_bedrooms',
+                'number_bath',
+                'area',
+                'property_address',
+                'price'
+            ])
+            writer.writerow(data)
+            print("export database to csv file successful ...")
+    except:
+        print("can not export database to csv file ...")
+    
         
 connection = sqlite3.connect('./feature_home.db')
 curser = connection.cursor()
@@ -39,4 +55,10 @@ curser.execute(sql)
 
 # connection.commit()
 # connection.close()
+
+# show all data in table feature home
 select_all_records(curser)
+
+# export all data table feature home to 
+data = curser.execute("SELECT * FROM feature_home")
+export_database_to_csv_file('output_data_feature_home.csv', data)
